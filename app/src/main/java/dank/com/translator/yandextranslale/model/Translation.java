fdb4f246c32d12f9918ddb8c8e249347929f16d0
Daniel Konovalenko <ddkono@gmail.com>
2017-03-18 16:55:32 +0300

    Add retrofit based YTranslate API and translation response model

---
+++ app/src/main/java/dank/com/translator/yandextranslale/model/Translation.java
@@ -0,0 +1,44 @@
+package dank.com.translator.yandextranslale.model;
+
+import java.util.List;
+
+import com.google.gson.annotations.SerializedName;
+import com.google.gson.annotations.Expose;
+
+public class Translation {
+
+    @SerializedName("code")
+    @Expose
+    private Integer code;
+    @SerializedName("lang")
+    @Expose
+    private String lang;
+    @SerializedName("text")
+    @Expose
+    private List<String> text = null;
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getLang() {
+        return lang;
+    }
+
+    public void setLang(String lang) {
+        this.lang = lang;
+    }
+
+    public List<String> getText() {
+        return text;
+    }
+
+    public void setText(List<String> text) {
+        this.text = text;
+    }
+
+}
