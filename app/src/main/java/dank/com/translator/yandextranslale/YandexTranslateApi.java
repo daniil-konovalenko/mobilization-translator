72931973e05be3afd3f096cd0fe633afd1e600d3
Daniel Konovalenko <ddkono@gmail.com>
2017-03-19 16:18:07 +0300

    Remove API key, which is now compromised and deactivated, fix translate call

--- app/src/main/java/dank/com/translator/yandextranslale/YandexTranslateApi.java
+++ app/src/main/java/dank/com/translator/yandextranslale/YandexTranslateApi.java
@@ -8,9 +8,8 @@ import retrofit2.http.Query;
 
 public interface YandexTranslateApi {
 
-    String API_KEY = "trnsl.1.1.20170314T182428Z.1c0f066b3fcc8f15.12d854b4a41add059d7b0b34504058bf7112ef1a";
 
-    @GET("/translate")
+    @GET("translate")
     Call<Translation> translate(@Query("key") String apiKey,
                                 @Query("text") String text,
                                 @Query("lang") String lang);
